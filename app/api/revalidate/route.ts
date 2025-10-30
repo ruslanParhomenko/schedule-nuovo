8ca603cdcf494265124ce13d6498f2d32a744d78
ruslan-parhomenko <parhomenkogm@gmail.com>
2025-10-30 13:56:32 +0200

    revalidate

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,19 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const secret = process.env.REVALIDATE_SECRET!;
+export async function POST(req: Request) {
+  try {
+    const tag = await req.json();
 
-  if (!body.secret || body.secret !== secret) {
-    return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
+    if (!tag) {
+      return new Response("Missing tag", { status: 400 });
+    }
+    //@ts-ignore
+    revalidateTag(tag);
+    return Response.json({ ok: true, tag });
+  } catch (error) {
+    return new Response("Error", { status: 500 });
   }
-
-  if (!body.tag) {
-    return NextResponse.json({ message: "Tag required" }, { status: 400 });
-  }
-  //@ts-ignore
-  revalidateTag(secret, body.tag);
-
-  return NextResponse.json({ message: `Tag ${body.tag} revalidated` });
 }
