3e0528019d1ccd789b5611e2e54e6c3704d530fe
ruslan-parhomenko <parhomenkogm@gmail.com>
2025-10-30 13:19:05 +0200

    revalidate

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const secret = process.env.REVALIDATE_SECRET;
+
+  if (!body.secret || body.secret !== secret) {
+    return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
+  }
+
+  if (!body.tag) {
+    return NextResponse.json({ message: "Tag required" }, { status: 400 });
+  }
+
+  revalidateTag(body.tag);
+
+  return NextResponse.json({ message: `Tag ${body.tag} revalidated` });
+}
